873b9f735c201c551f9a44c347702546943d5dfd
Joseph Daniszewski <daniszewski.joseph@gmail.com>
2024-12-05 18:47:26 +0900

    finished cart function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+import React, { useState } from "react";
+import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -13,33 +10,60 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
+    <>
+      <div className="background-image bg-slate-500 w-screen -z-10"></div>
+      {/* landing page */}
+      <div
+        className={`relative flex flex-col items-center justify-center md:flex-row pt-16 px-10 h-screen ${
+          showProductList ? "hidden" : ""
+        }`}
+      >
+        <div className="flex flex-col items-center justify-center w-full space-y-10 text-xl text-white md:space-y-4">
+          <h1 className="text-5xl font-bold text-center">
+            Welcome To Paradise Nursery
+          </h1>
+          <hr className="border-t-2 border-green-500 w-1/3" />
+          <p className="text-xl text-center">Where green meets serenity!</p>
+          <button
+            onClick={handleGetStartedClick}
+            className="bg-green-500 hover:bg-green-700 text-white  py-2 px-4 rounded"
+          >
             Get Started
           </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
-
+        </div>
+        <div className={`mx-16 pt-10 ${showProductList ? "hidden" : ""}`}>
+          <p className="text-xl text-white text-center pb-6">
+            Welcome to Paradise Nursery, where green meets serenity!{" "}
+          </p>
+          <p className="text-xl text-white pb-6 hidden md:block">
+            At Paradise Nursery, we are passionate about bringing nature closer
+            to you. Our mission is to provide a wide range of high-quality
+            plants that not only enhance the beauty of your surroundings but
+            also contribute to a healthier and more sustainable lifestyle. From
+            air-purifying plants to aromatic fragrant ones, we have something
+            for every plant enthusiast.
+          </p>
+          <p className="text-xl text-white hidden md:block">
+            Our team of experts is dedicated to ensuring that each plant meets
+            our strict standards of quality and care. Whether you're a seasoned
+            gardener or just starting your green journey, we're here to support
+            you every step of the way. Feel free to explore our collection, ask
+            questions, and let us help you find the perfect plant for your home
+            or office. Join us in our mission to create a greener, healthier
+            world. Visit Paradise Nursery today and experience the beauty of
+            nature right at your doorstep.
+          </p>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+      <div
+        className={`absolute w-full h-svh bg-gray-300 bg-opacity-50 overflow-auto z-10 top-0 transform transition-transform duration-500 ${
+          showProductList ? "translate-y-0" : "translate-y-full"
+        }`}
+      >
         <ProductList />
       </div>
-    </div>
+    </>
   );
 }
 
 export default App;
-
-
-
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,68 +1,118 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
-import './CartItem.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeItem, updateQuantity } from "./CartSlice";
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
+  const cart = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+      total += parseInt(item.cost.replace("$", "")) * item.quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({ name: item.name, quantity: item.quantity - 1 })
+      );
+    } else {
+      dispatch(removeItem(item.name));
+    }
+  };
+  const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
-  const handleRemove = (item) => {
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference");
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.replace("$", ""));
+    return item.quantity * cost;
   };
 
   return (
-    <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
-      <div>
-        {cart.map(item => (
-          <div className="cart-item" key={item.name}>
-            <img className="cart-item-image" src={item.image} alt={item.name} />
-            <div className="cart-item-details">
-              <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-cost">{item.cost}</div>
-              <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
-                <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+    <div className="flex flex-col gap-2 bg-white bg-opacity-80 items-center rounded-lg">
+      <h2 className="text-gray-800 mt-5 text-2xl">
+        Total Cart Amount: ${calculateTotalAmount()}
+      </h2>
+      <div className="w-full">
+        {cart.map((item) => (
+          <div
+            className="flex max-w-2xl flex-col md:flex-row border-b bg-white w-3/4 space-y-4 items-center mx-auto first:rounded-t-lg last:rounded-b-lg pt-3 pb-4"
+            key={item.name}
+          >
+            <img
+              className="flex h-52 w-52 rounded-r-md"
+              src={item.image}
+              alt={item.name}
+            />
+            <div className="justify-items-center mx-auto space-y-3">
+              <div className="text-xl font-semibold">{item.name}</div>
+              <div className="text-bold">{item.cost}</div>
+              <div className="flex flex-row space-x-5 items-center">
+                <button
+                  className="bg-gray-200 border border-gray-300 shadow-lg h-8 w-8 rounded-md text-sm hover:bg-gray-300"
+                  onClick={() => handleDecrement(item)}
+                >
+                  -
+                </button>
+                <span className="cart-item-quantity-value">
+                  {item.quantity}
+                </span>
+                <button
+                  className="bg-gray-200 border border-gray-300 shadow-lg h-8 w-8 rounded-md text-sm hover:bg-gray-300"
+                  onClick={() => handleIncrement(item)}
+                >
+                  +
+                </button>
               </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <div className="cart-item-total">
+                Total: ${calculateTotalCost(item)}
+              </div>
+              <button
+                className=" bg-red-500 rounded-md px-5 py-2 text-white hover:bg-red-600 shadow-lg transition-300"
+                onClick={() => handleRemove(item)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
-      <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+      <div className="mt-5 text-gray-950"></div>
+      <div className="flex flex-col justify-center w-1/2 md:w-1/4  pb-7">
+        <button
+          className="bg-yellow-500 rounded-md px-5 py-2 text-gray-950 hover:bg-yellow-600 shadow-lg transition-300"
+          onClick={(e) => handleContinueShopping(e)}
+        >
+          Continue Shopping
+        </button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button
+          className=" bg-green-500 rounded-md px-5 py-2 text-white hover:bg-green-600 shadow-lg transition-300"
+          onClick={(e) => handleCheckoutShopping(e)}
+        >
+          Checkout
+        </button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
